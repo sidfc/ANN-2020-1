97a7eaaa73ad44b14227262810962e087699b93c
sidfc <sidneifco@gmail.com>
2020-09-22 14:30:14 -0300

    implementation Runge-Kutta 4

--- edo/edo.py
+++ edo/edo.py
@@ -5,7 +5,7 @@ import numpy as np
 
 x0, y0 = 1, 2
 h = 0.125 # tamanho do passo
-n = 50
+n = 100
 
 def f(x, y):
     return 2 * y + x + 1
@@ -13,7 +13,7 @@ def f(x, y):
 def solucao_do_pvi(x):
     return (1/4) * (-2 * x + 13 * np.exp(2 * x - 2) - 3)
 
-def euler(f, x0, y0, h):
+def euler(f, x0, y0, h, n):
     x = {0: x0}
     y = {0: y0}
     for i in range(1, n):
@@ -21,7 +21,7 @@ def euler(f, x0, y0, h):
         y[i] = y[i - 1] + f(x[i - 1], y[i - 1]) * h # <- fórmula do método
     return x, y
 
-def heun(f, x0, y0, h):
+def heun(f, x0, y0, h, n):
     x = {i: x0 + i * h for i in range(n)}
     y = {0: y0}
     for k in range(n - 1):
@@ -30,25 +30,25 @@ def heun(f, x0, y0, h):
         y[k + 1] = y[k] + (h / 2) * (m1 + m2) # <- fórmula do método de Heun
     return x, y
 
-def euler_mid(f, x0, y0, h):
+def euler_mid(f, x0, y0, h, n):
     x = {i: x0 + i * h for i in range(n)}
     y = {0: y0}
     for k in range(n - 1):
         m1 = f(x[k], y[k])
-        m2 = f(x[k] + h, y[k] + m1 * h)
+        m2 = f(x[k] + h / 2, y[k] + m1 * h / 2)
         y[k + 1] = y[k] + h * m2 # <- fórmula do método de Euler do ponto médio
     return x, y
 
-def ralston(f, x0, y0, h):
+def ralston(f, x0, y0, h, n):
     x = {i: x0 + i * h for i in range(n)}
     y = {0: y0}
     for k in range(n - 1):
         m1 = f(x[k], y[k])
         m2 = f(x[k] + h, y[k] + m1 * h)
-        y[k + 1] = y[k] + (h / 3) * (1 * m1 + 2 * m2) # <- fórmula do método de Heun
+        y[k + 1] = y[k] + (h / 3) * (1 * m1 + 2 * m2) # <- fórmula do método de Ralston
     return x, y
 
-def ralston(f, x0, y0,c2, h):
+def rk2(f, x0, y0,c2, h, n):
     # c1 + c2 = 1 --> c1 = 1 - c2, c2 > 0
     c1 = 1 - c2
     x = {i: x0 + i * h for i in range(n)}
@@ -56,21 +56,32 @@ def ralston(f, x0, y0,c2, h):
     for k in range(n - 1):
         m1 = f(x[k], y[k])
         m2 = f(x[k] + h, y[k] + m1 * h)
-        y[k + 1] = y[k] + h * (c1 * m1 + c2 * m2) # <- fórmula do método de Heun
+        y[k + 1] = y[k] + h * (c1 * m1 + c2 * m2) # <- fórmula do método de RK2
     return x, y
 
-xs, ys = euler(f, x0, y0, h)
-x = [v for _, v in xs.items()]
-y = [v for _, v in ys.items()]
+def rk4(f, x0, y0, h, n):
+    x = {i: x0 + i * h for i in range(n)}
+    y = {0: y0}
+    for k in range(n - 1):
+        m1 = f(x[k], y[k])
+        m2 = f(x[k] + h / 2, y[k] + m1 * h / 2)
+        m3 = f(x[k] + h / 2, y[k] + m2 * h / 2)
+        m4 = f(x[k] + h, y[k] + m3 * h)
+        y[k + 1] = y[k] + (h / 6) * (m1 + 2 * m2 + 2 * m3 + m4) # <- fórmula do método de Runge-Kutta
+    return x, y
 
-hxs, hys = heun(f, x0, y0, h)
-hx = [v for _, v in hxs.items()]
-hy = [v for _, v in hys.items()]
+metodos = [euler, heun, euler_mid, ralston, rk4]
+names = ['Euler', 'Heun', 'Euler médio', 'Ralston', 'Runge-Kutta 4']
 
 t = np.linspace(x0, x0 + n * h, 100)
 
-plt.scatter(x, y, label="Euler")
-plt.scatter(hx, hy, label="Heun")
-plt.plot(t, solucao_do_pvi(t))
+for name, metodo in zip(names, metodos):
+    xs, ys = metodo(f, x0, y0, h, n)
+    x = list(xs.values())
+    y = list(ys.values())
+
+    plt.scatter(x, y, label=name)
+    plt.plot(t, solucao_do_pvi(t))
+
 plt.legend()
 plt.show()
